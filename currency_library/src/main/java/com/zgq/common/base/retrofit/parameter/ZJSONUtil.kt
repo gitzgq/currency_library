93dc20bb29e31a1aa09914699e98052d4facd425
zhangguoqing <zhangguoqing@ctypro.com>
2019-11-29 12:07:06 +0800

    完善JSONObject工具类

--- currency_library/src/main/java/com/zgq/common/base/retrofit/parameter/ZJSONUtil.kt
+++ currency_library/src/main/java/com/zgq/common/base/retrofit/parameter/ZJSONUtil.kt
@@ -17,63 +17,66 @@ class ZJSONUtil {
     /**
      * 拼接JSONObject
      */
-    fun put(o : JSONObject, key : String, value : Any) {
-        if (null == o || ZStringUtil.isEmpty(key) || null == value) {
-            return
+    fun put(o : JSONObject?, key : String?, value : Any?) {
+        o?.let {
+            if(!ZStringUtil.isEmpty(key)){
+                value?.let {it1->
+                    it.put(key, it1)
+                }
+            }
         }
-        o.put(key, value)
-        return
     }
 
     /**
      * 拼接JSONArray
      */
-    fun put(a : JSONArray, value : Any){
-        if (null == a || null == value) {
-            return
+    fun put(a : JSONArray?, value : Any?){
+        a?.let {
+            value?.let {it1->
+                put(a, it1)
+            }
         }
-        put(a, value)
     }
 
     /**
      * 判断是否包含这个key，并获取key对应的值
      */
-    fun hasObject(jsonObjec: JSONObject?, key : String) : JSONObject{
+    fun hasObject(jsonObjec: JSONObject?, key : String?) : JSONObject{
         if(isHas(jsonObjec, key)){
             return jsonObjec?.getJSONObject(key) ?: JSONObject()
         }
         return JSONObject()
     }
 
-    fun hasString(jsonObjec: JSONObject?, key : String) : String{
+    fun hasString(jsonObjec: JSONObject?, key : String?) : String{
         if(isHas(jsonObjec, key)){
             return jsonObjec?.getString(key) ?: ""
         }
         return ""
     }
 
-    fun hasInt(jsonObjec: JSONObject?, key : String) : Int{
+    fun hasInt(jsonObjec: JSONObject?, key : String?) : Int{
         if(isHas(jsonObjec, key)){
             return jsonObjec?.getInt(key) ?: 0
         }
         return 0
     }
 
-    fun hasDouble(jsonObjec: JSONObject?, key : String) : Double{
+    fun hasDouble(jsonObjec: JSONObject?, key : String?) : Double{
         if(isHas(jsonObjec, key)){
             return jsonObjec?.getDouble(key) ?: 0.0
         }
         return 0.0
     }
 
-    fun hasBoolean(jsonObjec: JSONObject?, key : String) : Boolean{
+    fun hasBoolean(jsonObjec: JSONObject?, key : String?) : Boolean{
         if(isHas(jsonObjec, key)){
             return jsonObjec?.getBoolean(key) ?: false
         }
         return false
     }
 
-    fun hasLong(jsonObjec: JSONObject?, key : String) : Long{
+    fun hasLong(jsonObjec: JSONObject?, key : String?) : Long{
         if(isHas(jsonObjec, key)){
             return jsonObjec?.getLong(key) ?: 0
         }
@@ -83,7 +86,7 @@ class ZJSONUtil {
     /**
      * 判断是否包含这个key
      */
-    fun isHas(jsonObjec: JSONObject?, key : String): Boolean {
+    fun isHas(jsonObjec: JSONObject?, key : String?): Boolean {
         return jsonObjec != null && !ZStringUtil.isEmpty(key) && jsonObjec.has(key)
     }
 
